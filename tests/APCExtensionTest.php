4799ab269295bfbeee65a5fbafb763aa1e996c8c
c9s <cornelius.howl@gmail.com>
2013-03-04 01:13:48 +0800

    Add bin file

---
+++ tests/APCExtensionTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class APCExtensionTest extends PHPUnit_Framework_ExtensionTestCase
+{
+
+    public function getExtensionName()
+    {
+        return 'apc';
+    }
+
+
+    public function test()
+    {
+        
+    }
+}
+
