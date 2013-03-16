bf2986ac006583ad0d892e21dc4445fa7600e8fc
c9s <cornelius.howl@gmail.com>
2013-03-17 01:16:16 +0800

    Improve PHPUnit_Framework_ExtensionTestCase test case

--- src/PHPUnit/Framework/ExtensionTestCase.php
+++ src/PHPUnit/Framework/ExtensionTestCase.php
@@ -2,42 +2,30 @@
 
 abstract class PHPUnit_Framework_ExtensionTestCase extends PHPUnit_Framework_TestCase
 {
-    abstract function getFunctions();
+    public function getFunctions() { return array(); }
+    public function getClasses() { return array(); }
 
-    public function getExtensionName() 
-    {
+    public function getExtensionName() {
     }
 
-    public function testExtensionLoad() 
+
+    public function testClasses()
     {
-        if( $name = $this->getExtensionName() ) {
-            $this->assertTrue( extension_loaded($name) );
+        $cs = $this->getClasses();
+        foreach( $cs as $c) {
+            $this->assertTrue( class_exists($c), "Check class existence for $c");
         }
+
     }
 
+    /**
+     * Make sure all defined function exists
+     */
     public function testFunctions() 
     {
         $functions = $this->getFunctions();
-        foreach( $functions as $function) {
-            $this->assertTrue( function_exists($function));
-        }
-    }
-
-    public function setUp()
-    {
-        if( $name = $this->getExtensionName() ) {
-            if( extension_loaded($name) ) {
-                // we can make tests
-
-
-            } else {
-                // skip if we are already in the child process
-                if( isset($_ENV['CHILD']) ) {
-                    return $this->markTestIncomplete("Can not load extension");
-                }
-                // launch process to re-run this phpunit test case
-
-            }
+        foreach( $functions as $f) {
+            $this->assertTrue( function_exists($f), "Check function existence for $f");
         }
     }
 }
